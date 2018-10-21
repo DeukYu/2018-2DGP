1553e982e51fb82980f9d3bd34ccb160571f1886
deuk yu lee(MSI) <ldy8070@naver.com>
2018-10-21 13:06:07 +0900

    Project CookieRun
    Interface competion
    character, pet running

--- Project/cookierun/main_lobby.py
+++ Project/cookierun/main_lobby.py
@@ -4,10 +4,10 @@ import game_framework
 
 name = "main_lobby"
 
-lobby = None
-start = None
-cookie = None
-pet = None
+imageLobby = None
+imageStartButton = None
+imageCookieButton = None
+imagePetButton = None
 
 
 class Lobby:
@@ -18,15 +18,18 @@ class Lobby:
         self.image.draw(400, 250)
 
 
-class Start_button:
+class StartButton:
+    SelectSB = 0
+
     def __init__(self):
-        self.image = load_image('start_button.png')
+            self.image = load_image('start_button.png')
 
     def draw(self):
-        self.image.draw(600, 40)
+        if SelectSB == 0:
+            self.image.draw(600, 40)
 
 
-class Cookie_selectbutton:
+class CookieSelectButton:
     def __init__(self):
         self.image = load_image('cookie_select.png')
 
@@ -34,7 +37,7 @@ class Cookie_selectbutton:
         self.image.draw(615, 120)
 
 
-class Pet_selectbutton:
+class PetSelectButton:
     def __init__(self):
         self.image = load_image('pet_select.png')
 
@@ -43,36 +46,42 @@ class Pet_selectbutton:
 
 
 def enter():
-    global lobby, start, cookie, pet
-    lobby = Lobby()
-    start = Start_button()
-    cookie = Cookie_selectbutton()
-    pet = Pet_selectbutton()
+    global imageLobby, imageStartButton, imageCookieButton, imagePetButton
+    imageLobby = Lobby()
+    imageStartButton = StartButton()
+    imageCookieButton = CookieSelectButton()
+    pet = PetSelectButton()
 
 
 def exit():
-    global lobby, start, cookie, pet
-    del(lobby)
-    del(start)
-    del(cookie)
-    del(pet)
+    global imageLobby, imageStartButton, imageCookieButton, imagePetButton
+    del(imageLobby)
+    del(imageStartButton)
+    del(imageCookieButton)
+    del(imagePetButton)
 
 
 def handle_events():
-   events = get_events()
-   for event in events:
-       if event.type == SDL_QUIT:
-           game_framework.quit()
-       elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-           game_framework.quit()
+    global x, y
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, 500 - event.y - 1
+
+
+x, y = 0, 0
 
 
 def draw():
     clear_canvas()
-    lobby.draw()
-    start.draw()
-    cookie.draw()
-    pet.draw()
+    imageLobby.draw()
+    imageStartButton.draw()
+    imageCookieButton.draw()
+    imagePetButton.draw()
     update_canvas()
 
 
