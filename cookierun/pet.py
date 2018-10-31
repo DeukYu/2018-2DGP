f47ab5cddba411b986cb97c45473af54ab9333dd
deuk yu lee(MSI) <ldy8070@naver.com>
2018-10-31 12:10:05 +0900

    펫 추가 구현
    펫 리소스 추가, 펫 (플라워콥터) 추가 구현

--- cookierun/pet.py
+++ cookierun/pet.py
@@ -7,7 +7,8 @@ import cookie
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-FRAMES_PER_ACTION = 4
+STAR_FRAMES_PER_ACTION = 4
+FLOWER_FRAMES_PER_ACTION = 7
 
 class Pet:
     def __init__(self):
@@ -17,6 +18,7 @@ class Pet:
         if interface_state.PetChoice == 0:
             self.image = load_image('resource/pet/starofangle_idle.png')
         elif interface_state.PetChoice == 1:
+            self.image = load_image('resource/pet/flower_idle.png')
             pass
         elif interface_state.PetChoice == 2:
             pass
@@ -24,10 +26,16 @@ class Pet:
             pass
 
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        if interface_state.PetChoice == 0:
+            self.frame = (self.frame + STAR_FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        elif interface_state.PetChoice == 1:
+            self.frame = (self.frame + FLOWER_FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 7
         self.y = main_state.cookie.y
 
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 48, 0, 48, 32, self.x, self.y)
-        draw_rectangle(self.x - 24, self.y + 16, self.x + 24, self.y - 16)
-
+        if interface_state.PetChoice == 0:
+            self.image.clip_draw(int(self.frame) * 48, 0, 48, 32, self.x, self.y)
+            draw_rectangle(self.x - 24, self.y + 16, self.x + 24, self.y - 16)
+        elif interface_state.PetChoice == 1:
+            self.image.clip_draw(int(self.frame) * 32, 0, 32, 32, self.x, self.y)
+            draw_rectangle(self.x - 16, self.y + 16, self.x + 16, self.y - 16)
