cb618686230ac1f9f0a6bec9653fbb40093eb5bf
deuk yu lee(MSI) <ldy8070@naver.com>
2018-10-31 13:01:17 +0900

    펫 수정
    펫 리소스 및 추가 구현

--- cookierun/pet.py
+++ cookierun/pet.py
@@ -7,35 +7,37 @@ import cookie
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
-STAR_FRAMES_PER_ACTION = 4
-FLOWER_FRAMES_PER_ACTION = 7
+STAR_FLOWER_FRAMES_PER_ACTION = 4
+GHOST_FRAMES_PER_ACTION = 3
 
 class Pet:
     def __init__(self):
-        self.x, self.y = 150, 102
+        self.x, self.y = 140, 102
         self.frame = 0
 
         if interface_state.PetChoice == 0:
             self.image = load_image('resource/pet/starofangle_idle.png')
         elif interface_state.PetChoice == 1:
             self.image = load_image('resource/pet/flower_idle.png')
-            pass
         elif interface_state.PetChoice == 2:
             pass
         elif interface_state.PetChoice == 3:
-            pass
+            self.image = load_image('resource/pet/ghost_idle.png')
 
     def update(self):
-        if interface_state.PetChoice == 0:
-            self.frame = (self.frame + STAR_FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
-        elif interface_state.PetChoice == 1:
-            self.frame = (self.frame + FLOWER_FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 7
-        self.y = main_state.cookie.y
+        if interface_state.PetChoice == 0 or 1:
+            self.frame = (self.frame + STAR_FLOWER_FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        elif interface_state.PetChoice == 3:
+            self.frame = (self.frame + GHOST_FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3
+        self.y = main_state.cookie.y + 10
 
     def draw(self):
         if interface_state.PetChoice == 0:
-            self.image.clip_draw(int(self.frame) * 48, 0, 48, 32, self.x, self.y)
-            draw_rectangle(self.x - 24, self.y + 16, self.x + 24, self.y - 16)
+            self.image.clip_draw(int(self.frame) * 96, 0, 96, 64, self.x, self.y)
+            draw_rectangle(self.x - 48, self.y + 32, self.x + 48, self.y - 32)
         elif interface_state.PetChoice == 1:
-            self.image.clip_draw(int(self.frame) * 32, 0, 32, 32, self.x, self.y)
-            draw_rectangle(self.x - 16, self.y + 16, self.x + 16, self.y - 16)
+            self.image.clip_draw(int(self.frame) * 48, 0, 48, 64, self.x, self.y)
+            draw_rectangle(self.x - 24, self.y + 32, self.x + 24, self.y - 32)
+        elif interface_state.PetChoice == 3:
+            self.image.clip_draw(int(self.frame) * 48, 0, 48, 48, self.x, self.y)
+            draw_rectangle(self.x - 24, self.y + 24, self.x + 24, self.y - 24)
