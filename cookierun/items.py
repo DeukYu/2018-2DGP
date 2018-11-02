3eb3180fbb8ce14b451ee046b6d95a1a6c1d4292
Deuk Yu <ldy8070@naver.com>
2018-11-03 00:43:53 +0900

    아이템 생성시 오류 해결
    int expected instead of float 오류 해결

--- cookierun/items.py
+++ cookierun/items.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+import cookie
 import game_framework
 import game_world
 import random
@@ -11,6 +12,7 @@ class Items:
     def __init__(self, select=0):
         self.select = select
         self.x = 800
+        self.frame = 0
         if self.select == 0: # 젤리
             self.image = load_image('resource/items/Jelly.png')
             self.y = random.randint(125, 450)
@@ -38,16 +40,19 @@ class Items:
     def update(self):
         self.x -= 100 * game_framework.frame_time
 
+        if self.select == 1 or self.select == 2:
+            self.frame = (self.frame + cookie.FRAMES_PER_ACTION4 * cookie.ACTION_PER_TIME * game_framework.frame_time) % 4
+
         if self.x + self.Left_Right < 0:
             game_world.remove_object(self)
 
     def draw(self):
         if self.select == 0:
-            self.image.clip_draw(0, 0, 24, 32, self.x, self.y, 24, 32)
+            self.image.clip_draw(self.frame * 0, 0, 24, 32, self.x, self.y, 24, 32)
             draw_rectangle(*self.get_bb())
         elif self.select == 1:
-            self.image.clip_draw(0, 0, 32, 32, self.x, self.y, 32, 32)
+            self.image.clip_draw(int(self.frame) * 32, 0, 32, 32, self.x, self.y, 32, 32)
             draw_rectangle(*self.get_bb())
         elif self.select == 2:
-            self.image.clip_draw(0, 0, 32, 32, self.x, self.y, 32, 32)
+            self.image.clip_draw(int(self.frame) * 32, 0, 32, 32, self.x, self.y, 32, 32)
             draw_rectangle(*self.get_bb())
